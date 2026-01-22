2fbe9a9cf6a66b837c15376550d08542a1fbc436
skr3d3 <skr3d3@gmail.com>
2026-01-23 00:16:44 +0100

    fix: wrap AppRouter with HashRouter for reliable ghpages routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AppProviders } from "./app/providers";
 import { AppRouter } from "./app/router";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AppProviders>
-      <AppRouter />
+      <HashRouter>
+        <AppRouter />
+      </HashRouter>
     </AppProviders>
   </React.StrictMode>
 );
